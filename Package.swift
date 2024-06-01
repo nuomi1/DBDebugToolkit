3c8b38ca38930cba4ec797c7c9648e4ad3174ef8
nuomi1 <nuomi1@qq.com>
2024-06-01 09:21:34 +0800

    feat: split the target to use a single language

--- Package.swift
+++ Package.swift
@@ -3,16 +3,68 @@ import PackageDescription
 
 let package = Package(
     name: "DBDebugToolkit",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "DBDebugToolkit",
-            targets: ["DBDebugToolkit"]),
+            targets: ["DBDebugToolkitObjectiveCCore"]
+        ),
     ],
     targets: [
         .target(
-            name: "DBDebugToolkit",
+            name: "DBDebugToolkitSwiftBase",
             path: "DBDebugToolkit",
-            publicHeadersPath: "Headers"),
+            exclude: [
+                "Classes/Extensions/DBRequestModel+responseDescription.swift",
+                "Resources",
+            ],
+            sources: [
+                "Classes/Extensions"
+            ]
+        ),
+        .target(
+            name: "DBDebugToolkitObjectiveCBase",
+            dependencies: [
+                .target(name: "DBDebugToolkitSwiftBase")
+            ],
+            path: "DBDebugToolkit",
+            exclude: [
+                "Classes/Extensions",
+                "Classes/Swift",
+                "Classes/DBDebugToolkit.m",
+            ],
+            sources: [
+                "Classes"
+            ],
+            publicHeadersPath: "Headers"
+        ),
+        .target(
+            name: "DBDebugToolkitSwiftCore",
+            dependencies: [
+                .target(name: "DBDebugToolkitObjectiveCBase"),
+            ],
+            path: "DBDebugToolkit",
+            exclude: [
+                "Resources",
+            ],
+            sources: [
+                "Classes/Extensions/DBRequestModel+responseDescription.swift",
+                "Classes/Swift",
+            ]
+        ),
+        .target(
+            name: "DBDebugToolkitObjectiveCCore",
+            dependencies: [
+                .target(name: "DBDebugToolkitSwiftBase"),
+                .target(name: "DBDebugToolkitSwiftCore"),
+            ],
+            path: "DBDebugToolkit",
+            exclude: [
+                "Resources",
+            ],
+            sources: [
+                "Classes/DBDebugToolkit.m",
+            ]
+        )
     ]
 )
